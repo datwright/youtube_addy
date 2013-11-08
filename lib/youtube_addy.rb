c7ba1b31d7c6e495a1e2cc053843f9573e99c61c
Rafael da Silva Almeida <eusou@rafaelalmeida.net>
2013-11-08 16:40:25 -0200

    Conversion into each youtube url type

--- lib/youtube_addy.rb
+++ lib/youtube_addy.rb
@@ -2,6 +2,7 @@ class YouTubeAddy
   URL_Formats = {
       regular: /^(https?:\/\/)?(www\.)?youtube.com\/watch\?(.*\&)?v=([^&]+)/,
       shortened: /^(https?:\/\/)?(www\.)?youtu.be\/([^&]+)/,
+      embed: /^(https?:\/\/)?(www\.)?youtube.com\/embed\/([^&]+)/,
       invalid_chars: /[^a-zA-Z0-9\:\/\?\=\&\$\-\_\.\+\!\*\'\(\)\,]/
   }
 
@@ -16,6 +17,8 @@ class YouTubeAddy
       return match[4]
     elsif match = URL_Formats[:shortened].match(youtube_url)
       return match[3]
+    elsif match = URL_Formats[:embed].match(youtube_url)
+      return match[3]
     end
 
     nil
@@ -24,5 +27,15 @@ class YouTubeAddy
   def self.youtube_embed_url(youtube_url, width = 420, height = 315)
     vid_id = extract_video_id(youtube_url)
     %(<iframe width="#{width}" height="#{height}" src="http://www.youtube.com/embed/#{vid_id}" frameborder="0" allowfullscreen></iframe>)
+  end
+
+  def self.youtube_regular_url(youtube_url)
+    vid_id = extract_video_id(youtube_url)
+    "http://www.youtube.com/watch?v=#{ vid_id }"
+  end
+
+  def self.youtube_shortened_url(youtube_url)
+    vid_id = extract_video_id(youtube_url)
+    "http://youtu.be/#{ vid_id }"
   end
 end
