8add24cd6d4ba1321913eaea8dc83d71a4356ab3
Rafael da Silva Almeida <eusou@rafaelalmeida.net>
2013-11-08 16:50:20 -0200

    Use Named Captures to remove magick numbers

--- lib/youtube_addy.rb
+++ lib/youtube_addy.rb
@@ -1,27 +1,23 @@
 class YouTubeAddy
-  URL_Formats = {
-      regular: /^(https?:\/\/)?(www\.)?youtube.com\/watch\?(.*\&)?v=([^&]+)/,
-      shortened: /^(https?:\/\/)?(www\.)?youtu.be\/([^&]+)/,
-      embed: /^(https?:\/\/)?(www\.)?youtube.com\/embed\/([^&]+)/,
-      invalid_chars: /[^a-zA-Z0-9\:\/\?\=\&\$\-\_\.\+\!\*\'\(\)\,]/
+  URL_FORMATS = {
+      regular: /^(https?:\/\/)?(www\.)?youtube.com\/watch\?(.*\&)?v=(?<id>[^&]+)/,
+      shortened: /^(https?:\/\/)?(www\.)?youtu.be\/(?<id>[^&]+)/,
+      embed: /^(https?:\/\/)?(www\.)?youtube.com\/embed\/(?<id>[^&]+)/
   }
 
+  INVALID_CHARS = /[^a-zA-Z0-9\:\/\?\=\&\$\-\_\.\+\!\*\'\(\)\,]/
+
   def self.has_invalid_chars?(youtube_url)
-    !URL_Formats[:invalid_chars].match(youtube_url).nil?
+    !INVALID_CHARS.match(youtube_url).nil?
   end
 
   def self.extract_video_id(youtube_url)
     return nil if has_invalid_chars?(youtube_url)
 
-    if match = URL_Formats[:regular].match(youtube_url)
-      return match[4]
-    elsif match = URL_Formats[:shortened].match(youtube_url)
-      return match[3]
-    elsif match = URL_Formats[:embed].match(youtube_url)
-      return match[3]
+    URL_FORMATS.values.each do |format_regex|
+      match = format_regex.match(youtube_url)
+      return match[:id] if match
     end
-
-    nil
   end
 
   def self.youtube_embed_url(youtube_url, width = 420, height = 315)
